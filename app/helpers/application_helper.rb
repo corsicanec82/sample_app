a5e88167d2dd79019ea8679404e6f7b4ab8c70e7
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-11-28 18:04:31 +0200

    add comment to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
   def full_title(page_title = '')
     base_title = 'Ruby on Rails Sample App'
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"

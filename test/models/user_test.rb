8d377ff13755cc951cb5f220cca8a04800142456
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-12-01 20:39:48 +0200

    add format validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,4 +28,22 @@ class UserTest < ActiveSupport::TestCase
     @user.email = 'a' * 244 + '@example.com'
     assert_not @user.valid?
   end
+
+  test 'email validation should accept valid addresses' do
+    valid_adresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                        first.last@foo.jp alice+bob@baz.cn]
+    valid_adresses.each do |valid_adress|
+      @user.email = valid_adress
+      assert @user.valid?, "#{valid_adress.inspect} should be valid"
+    end
+  end
+
+  test 'email validation should reject invalid addresses' do
+    invalid_adresses = %w[user@example,com user_at_foo.org user.name@example.
+                          foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_adresses.each do |invalid_adress|
+      @user.email = invalid_adress
+      assert_not @user.valid?, "#{invalid_adress.inspect} should be invalid"
+    end
+  end
 end
